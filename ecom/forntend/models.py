6c2bf234694cc49dada1ebd70b5ca3618a06d90e
pankajpandey1393 <114969913+pankajpandey1393@users.noreply.github.com>
2022-10-08 02:20:54 -0700

    Delete ecom directory

--- ecom/forntend/models.py
+++
@@ -1,9 +0,0 @@
-from distutils.command.upload import upload
-from django.db import models
-
-class product(models.Model):
-    productname =models.CharField(max_length=280)
-    price=models.IntegerField(max_length=5)
-    description=models.TextField()
-    image=models.FileField(upload_to='products')
-
